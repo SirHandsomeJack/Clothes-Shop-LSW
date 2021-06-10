cf647a7b50ae9694f72631381fbca15dd11b2f28
Jack Young <SirHandsomeJack@gmail.com>
2021-06-10 07:45:53 +0100

    Added Escape menu to quit the game

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -25,6 +25,9 @@ public class GameManager : PersistentSingleton<GameManager>
             case "UnPause":
                 Pause(false);
                 break;
+            case "Quit":
+                Application.Quit();
+                break;
         }
     }
 
